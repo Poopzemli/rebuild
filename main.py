8d9bc01112b19d4ffcc22809ddab0d59fb84d005
Danil Ketov <ketov-x@inbox.ru>
2018-11-09 20:47:40 +0300

    Наконец подгрузка таблицы работает! Мы так долго ждали эту функцию! Так же появилась функция отправки слова "CLICKED!" в консоль при нажатии кнопки информации у обьектов!

--- main.py
+++ main.py
@@ -30,6 +30,7 @@ class ExapleApp (QtWidgets.QMainWindow, design.Ui_MainWindow):
             nf = QtWidgets.QFrame()
             nf.setStyleSheet(".QLabel { max-height: 170; max-width: 170; min-width: 170; min-height: 170; border: 1px solid white; }")
             nb = QtWidgets.QPushButton(nf)
+            nb.clicked.connect(lambda: self.DFrameSendInfo(df.ID))
             nb.setText("Информация")
             nb.move(35, 190)
 
@@ -51,7 +52,7 @@ class ExapleApp (QtWidgets.QMainWindow, design.Ui_MainWindow):
         nb.setText("Информация")
         nb.move(35, 190)
 
-        pic, addr, plosh, kom, type, price, index, ok = self.GetAddInformation()
+        pic, addr, plosh, kom, tip, price, index, ok = self.GetAddInformation()
 
         if ok:
             nl = QtWidgets.QLabel(nf)
@@ -64,8 +65,16 @@ class ExapleApp (QtWidgets.QMainWindow, design.Ui_MainWindow):
             if self.NewPositionH == 4:
                 self.NewPositionV += 1
                 self.NewPositionH = 0
+        df = pd.read_csv(self.DBase + self.DFile, sep=',', header=0, encoding='utf-8', index_col=False)
+        new_line = pd.DataFrame({'ID':len(df.index)+1, 'Адрес':addr, 'Площадь':plosh, 'Комнаты':kom, 'Тип сделки':tip, 'Цена':price, 'Индекс':index, 'pic':pic}, index=range(1))
+        print(new_line)
+        nb.clicked.connect(lambda : self.DFrameSendInfo(df.ID))
+        df = df.append(new_line)
+        print(df)
+        df.to_csv(self.DBase + self.DFile, sep=',', encoding='utf-8', line_terminator='\n', index=False)
 
-
+    def DFrameSendInfo(self, ID):
+        print('CLICKED!')
     def GetAddInformation(self):
         pic, ok = QtWidgets.QInputDialog().getText(self, 'InputText', 'Адрес изображения:?')
         if ok:
